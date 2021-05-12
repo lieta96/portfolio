811d5277316477ce9f1524375d9e867cb4d33293
Julieta Garcia <julieta.garcia96@gmail.com>
2021-05-12 17:18:57 -0300

    nueva sección de proyectos

--- src/Components/ProjectsSection/ProjectsSection.jsx
+++ src/Components/ProjectsSection/ProjectsSection.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import projects from './projects.js'
+import './ProjectsSection.css'
 
 function ProjectsSection (){
     return (
-        <div>
+        <div className="projects-section-container">
             <h2>Proyectos</h2>
             <div className='projects-container'>
                 {projects.map((proyecto)=>{return (
                     <div className='project-container'>
                         <h3>{proyecto.name}</h3>
                         <img src={proyecto.imageURL} alt={proyecto.name} />
-                        <span>Repo</span>
-                        <span>Demo</span>
+                        <div className="repository-container">
+                            <span> {`</>`} </span>
+                            <i class="fas fa-eye"></i>
+                        </div>
+                        
                
                     </div>
                  )}
--- src/Components/ProjectsSection/projects.js
+++ src/Components/ProjectsSection/projects.js
@@ -6,5 +6,26 @@ const projects=[
         repo: 'https://github.com/lieta96/todo-app-main-challenge',
         demo:'https://lieta96.github.io/todo-app-main-challenge/',
     },
+    {
+        name:'Organic Store',
+        imageURL:'https://raw.githubusercontent.com/lieta96/organic-store/main/public/organic-store.jpg',
+        description:'',
+        repo: 'https://github.com/lieta96/organic-store',
+        demo:'https://lieta96.github.io/organic-store/',
+    },
+    {
+        name:'IP Adress Tracker',
+        imageURL:'https://raw.githubusercontent.com/lieta96/ip-adress-tracker-master-challenge/main/design/desktop-design.jpg',
+        description:'',
+        repo: 'https://github.com/lieta96/ip-adress-tracker-master-challenge',
+        demo:'https://lieta96.github.io/ip-adress-tracker-master-challenge/',
+    },
+    {
+        name:'Launch Countdown',
+        imageURL:'https://raw.githubusercontent.com/lieta96/launch-countdown-challenge/main/design/desktop-preview.jpg',
+        description:'',
+        repo: 'https://github.com/lieta96/launch-countdown-challenge',
+        demo:'https://lieta96.github.io/launch-countdown-challenge/',
+    }
 ]
 export default projects
